ca736438d82404cdee99aaad990e403e2578114c
zsocky <zsoltpaskuy18@gmail.com>
2024-03-01 21:31:31 +0200

    fix: Added core modules for testing with frontend API

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import helmet from 'helmet';
 import { routes } from "./routes/routes.js";
 import { startCronJob } from './cron/tokenCleanup.js';
@@ -7,6 +8,10 @@ import { sanitizeInput } from './middleware/sanitizeInput.js';
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
 
 app.use(
     helmet({
