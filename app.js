9e2164e632abc273f94f6badfc2647789cc6e97e
zsocky <zsoltpaskuy18@gmail.com>
2024-12-17 17:08:18 +0200

    Fix: Restructure crsf middleware to more secure cookie options.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import { routes } from './routes/routes.js';
 import { startCronJob } from './cron/tokenCleanup.js';
 import { sanitizeInput } from './middleware/sanitizeInput.js';
 import helmetMiddleware from './middleware/helmetMiddleware.js';
-import csrfEnhancedMiddleware from './middleware/csrfMiddleware.js';
 
 const app = express();
 
@@ -13,10 +12,7 @@ app.use(corsMiddleware); // CORS Middleware
 app.use(helmetMiddleware); // Helmet Middleware for security
 app.use(sanitizeInput); // Input Sanitization Middleware
 
-app.options('*', corsMiddleware); // Explicitly handle OPTIONS requests
-
-// Apply enhanced CSRF middleware
-// app.use(csrfEnhancedMiddleware);
+app.options('*', corsMiddleware);
 
 routes(app); // Define your routes
 
--- middleware/csrfMiddleware.js
+++ middleware/csrfMiddleware.js
@@ -1,15 +1,20 @@
 import csrf from 'csurf';
 
 // Initialize CSRF middleware with cookie option
-const csrfMiddleware = csrf({ cookie: true });
+const csrfMiddleware = csrf({
+  cookie: {
+      httpOnly: true,
+      secure: true, // Use secure cookies
+      sameSite: 'strict', // Prevent CSRF token leakage
+  },
+});
 
-// Wrapper middleware to apply CSRF and expose the token as a cookie
 const csrfEnhancedMiddleware = (req, res, next) => {
   csrfMiddleware(req, res, (err) => {
     if (err) {
-      // Handle CSRF errors if necessary (e.g., logging or custom error response)
       return next(err);
     }
+
     // Set the CSRF token as a cookie for the frontend
     res.cookie('XSRF-TOKEN', req.csrfToken());
     next();
