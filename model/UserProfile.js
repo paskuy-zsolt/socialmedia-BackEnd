214124389cc63a3e3b097fb9e332f7cdfe476d6d
zsocky <zsoltpaskuy18@gmail.com>
2025-02-13 16:26:20 +0200

    feat: add UserProfiles.js with user profile schema and validation
    - Add new file `UserProfiles.js` to define the user profile schema using Mongoose.
    - Include regex validation for email and phone fields.
    - Define schema fields: `userID`, `name`, `email`, `avatar`, `username`, `phone`, and `description`.
    - Add validation rules for required fields, length constraints, and format matching.
    - Enable timestamps for automatic `createdAt` and `updatedAt` fields.

---
+++ model/UserProfile.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+export const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+export const phoneRegex = /^\+?[1-9]\d{1,14}$/;
+
+const { Schema } = mongoose;
+
+const userProfileSchema = new Schema({
+  userID: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  name: {
+    type: String,
+    required: [true, "Name is required!"],
+    minlength: [3, "Name must have at least 3 letters"],
+    maxlength: [200, "Name is too long"]
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email is required!"],
+    maxlength: [100, "Email is too long"],
+    match: [emailRegex, "The email format is not supported"],
+    lowercase: true,
+    trim: true
+  },
+  avatar: {
+    type: String,
+    default: "",
+    required: false
+  },
+  username: {
+    type: String,
+    trim: true,
+    maxlength: [50, "Nickname is too long"],
+    required: false,
+    sparse: true,
+  },  
+  phone: {
+    type: String,
+    match: [phoneRegex, "Phone number must follow E.164 format (+123456789)"],
+    required: false
+  },
+  description: {
+    type: String,
+    maxlength: [500, "Description is too long"],
+    required: false
+  },
+}, { timestamps: true });
+
+export default mongoose.model("UserProfile", userProfileSchema);
